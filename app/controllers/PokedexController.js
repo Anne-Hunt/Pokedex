d31177647f24f9c8bdbd78969c6eba4cd48672f2
Anne Hunt <acathunt@gmail.com>
2024-04-03 14:02:22 -0600

    service jobs listed, API accessed, controller skeleton

--- app/controllers/PokedexController.js
+++ app/controllers/PokedexController.js
@@ -1,10 +1,38 @@
+import { AppState } from "../AppState.js"
+import { pokedexService } from "../services/PokedexService.js"
+import { Pop } from "../utils/Pop.js"
 
 
 export class PokedexController {
     constructor() {
+        this.getPokeData()
+        AppState.on('pokemons', this.drawPokeList)
+        AppState.on('activePokemon', this.drawActivePoke)
+    }
+
+    async getPokeData() {
+        try {
+            console.log('pokedata starting')
+            const response = await pokedexService.getPokeData()
+        } catch (error) {
+            console.log(error)
+            Pop.toast("Oh no, Pokedudes are unable to be found", 'error')
+        }
+    }
+
+    async setActivePoke(pokeId) {
+        console.log('sending request to service', pokeId)
 
     }
 
+    drawActivePoke(pokeId) {
+        console.log('drawing pokemon', pokeId)
+
+    }
+
+    drawPokeList() {
+        console.log('making em all appear')
+    }
 
 
 }
--- app/services/PokedexService.js
+++ app/services/PokedexService.js
@@ -1,6 +1,34 @@
+import { AppState } from "../AppState.js"
+import { Pop } from "../utils/Pop.js"
 
+// eslint-disable-next-line no-undef
+// @ts-ignore
+const PokemonAPI = axios.create({ // this will always give you an error, because axios only exists once the page loads
+    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=100',
+    timeout: 8000
+})
 
 class PokedexService {
+
+    async getPokeData() {
+        try {
+            const response = await PokemonAPI.get('PokemonAPI')
+            console.log('caughtmons', response)
+            AppState.pokemons = response.data.results
+            console.log('AppStatedcaughtmons', AppState.pokemons)
+        } catch (error) {
+            console.log(error)
+            Pop.toast("Where have all the pokedudes gone?", "error")
+        }
+    }
+
+    async setActivePoke() {
+
+    }
+
+
+
+
 
 }
 
